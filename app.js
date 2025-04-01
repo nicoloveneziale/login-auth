64b47c1ed906f9b1e508ba62122495e1e6fc0adf
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-04-01 16:09:07 +0100

    Added an app.js file

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const pool = new Pool({
+  connectionString: "postgresql://nicolo:pgpass@localhost:5432/users_practice",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
